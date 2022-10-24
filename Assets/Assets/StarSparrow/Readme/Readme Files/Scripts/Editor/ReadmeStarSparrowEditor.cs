9b808494c42ba979e78d27322144c32db48b2a4e
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-24 14:49:43 +0200

    Added spaceship player model to the game and made a simple timeline for the player and placeholder enemy

---
+++ Assets/Assets/StarSparrow/Readme/Readme Files/Scripts/Editor/ReadmeStarSparrowEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f46486324a47a1af19df4255694893431230e0d816ac8613f02dd5fa6e4da33
+size 4256
---
+++ Assets/Assets/StarSparrow/Readme/Readme Files/Scripts/ReadmeStarSparrow.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c20b465683333c8f7409be41658b9501d505f19852a383f7747e518497b3861
+size 297
