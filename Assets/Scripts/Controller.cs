84279f9b36bc445cb064d24dc31e4ca6d2850985
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-24 18:15:09 +0200

    Made player be able to move using WASD with the new input system

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1801e6d3928b7cbe35bcfe92aff7c920ce34d6bda573f1d24c2d1f93fdf26dd1
+size 1441
---
+++ Assets/Scripts/Input system/Controls.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c732dbd71934ecfded2f00fd3db59b2c4ea5e4b343c7741b567820c24bcac71
+size 7692
