c91acce54f902c7fb75fd7e9a5a9ac1119db82fa
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-29 16:19:15 +0200

    Added simple hit point system and added new enemy

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d98ce59089aca655709adf8fbe2b20bf426f00f19343ee76c7b2e368de33162
+size 331
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:778de674b56892dc18f2f5f51812e787f1abd8682ca3c770e109a97c76da36b3
-size 999
+oid sha256:a36d2ed8437d5f5057473006dc447fdc31ae83a225cad3b257bfd078c283bc1f
+size 1674
